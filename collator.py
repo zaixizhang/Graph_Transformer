d092e086bbe7c2ad66c42fe36b90388271d63705
zaixizhang <52197241+zaixizhang@users.noreply.github.com>
2022-10-28 10:05:05 +0800

    Update collator.py

--- collator.py
+++ collator.py
@@ -2,7 +2,6 @@ import torch
 
 
 def pad_1d_unsqueeze(x, padlen):
-    x = x + 1  # pad id = 0
     xlen = x.size(0)
     if xlen < padlen:
         new_x = x.new_zeros([padlen], dtype=x.dtype)
@@ -12,7 +11,6 @@ def pad_1d_unsqueeze(x, padlen):
 
 
 def pad_2d_unsqueeze(x, padlen):
-    x = x + 1  # pad id = 0
     xlen, xdim = x.size()
     if xlen < padlen:
         new_x = x.new_zeros([padlen, xdim], dtype=x.dtype)
@@ -42,7 +40,6 @@ def pad_edge_type_unsqueeze(x, padlen):
 
 
 def pad_spatial_pos_unsqueeze(x, padlen):
-    x = x + 1
     xlen = x.size(0)
     if xlen < padlen:
         new_x = x.new_zeros([padlen, padlen], dtype=x.dtype)
@@ -52,7 +49,6 @@ def pad_spatial_pos_unsqueeze(x, padlen):
 
 
 def pad_3d_unsqueeze(x, padlen1, padlen2, padlen3):
-    x = x + 1
     xlen1, xlen2, xlen3, xlen4 = x.size()
     if xlen1 < padlen1 or xlen2 < padlen2 or xlen3 < padlen3:
         new_x = x.new_zeros([padlen1, padlen2, padlen3, xlen4], dtype=x.dtype)
